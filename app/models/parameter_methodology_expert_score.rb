03bc0ee705a1e10d9e9de297079d71f43b2ee8c9
Anna <akhsunna4@gmail.com>
2020-11-26 17:22:58 +0200

    fix

--- app/models/parameter_methodology_expert_score.rb
+++ app/models/parameter_methodology_expert_score.rb
@@ -7,24 +7,31 @@ class ParameterMethodologyExpertScore < ApplicationRecord
 
   belongs_to :parameter_value
   belongs_to :methodology
-  belongs_to :expert_request
+  belongs_to :expert_request, optional: true
   has_one :parameter, through: :parameter_value
-
-  scope :for_parameter, ->(parameter_id) { joins(:parameter_value).where(parameter_values: { parameter_id: parameter_id }) }
 
   validates :score, :expert_weight, presence: true, if: -> { confirmed? }
   validates :score, :expert_weight, numericality: {
-    only_integer: true,
     greater_than_or_equal_to: 0,
     less_than_or_equal_to: 10
   }, allow_blank: true
 
+  after_save :update_total, if: ->{ confirmed? }
+
+  scope :for_parameter, ->(parameter_id) { joins(:parameter_value).where(parameter_values: { parameter_id: parameter_id }) }
+
   def update_total!
-    total_record = ParameterMethodologyTotalScore.find_or_initialize_by(methodology: methodology, parameter_value: parameter_value)
+    total_record = ParameterMethodologyTotalScore.find_or_create_by(methodology: methodology, parameter_value: parameter_value, score: 0)
     total_record.update_score!
   end
 
   def weighted_score
-    score * expert_weight / 10.0
+    score * (expert_weight || 1) / 10.0
+  end
+
+  private
+
+  def update_total
+    update_total!
   end
 end
